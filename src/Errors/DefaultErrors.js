8e07a22522ec3390b564b7f9aa2a0b8915c39511
jrbigmon <junioterriaga0@gmail.com>
2022-08-13 19:34:04 -0300

    Created pattern errors

---
+++ src/Errors/DefaultErrors.js
@@ -0,0 +1,17 @@
+module.exports = {
+    ExistsInDatase: {
+        error: true,
+        message: 'Exists in database already!'
+    },
+
+    NotExistsInDatase: {
+        error: true,
+        message: 'Not Exists in database already!'
+    },
+
+    DatabaseOut: {
+        error: true,
+        message: 'The system is out now!'
+    }
+
+}
