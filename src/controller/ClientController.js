503843542c088564823e73ab01a0bba1658bba51
jrbigmon <junioterriaga0@gmail.com>
2022-08-16 19:29:48 -0300

    Change patter if by one line

--- src/controller/ClientController.js
+++ src/controller/ClientController.js
@@ -7,9 +7,7 @@ const ClientController = {
     try {
       const { id } = req.params
       const client = await Client.findByPk(id)
-      if (!client) {
-        return res.status(400).json(DefaultErrors.NotExistsInDatase)
-      }
+      if (!client) return res.status(400).json(DefaultErrors.NotExistsInDatase)
       return res.json(client)
     } catch (err) {
       return res.status(500).json(DefaultErrors.DatabaseOut)
@@ -41,9 +39,7 @@ const ClientController = {
         password: bcrypt.hashSync(password, 10)
       }
       const verifyIfExists = await Client.findOne({ where: { email } })
-      if (verifyIfExists) {
-        return res.status(409).json(DefaultErrors.ExistsInDatase)
-      }
+      if (verifyIfExists)  return res.status(409).json(DefaultErrors.ExistsInDatase)
       await Client.create(newClient)
       return res.status(201).json(newClient)
     } catch (err) {
@@ -66,9 +62,7 @@ const ClientController = {
       // const { avatar } = req.file
       const verifyIfExists = await Client.findByPk(id)
       // console.log(verifyIfExists)
-      if (!verifyIfExists) {
-        return res.status(400).json(DefaultErrors.NotExistsInDatase)
-      }
+      if (!verifyIfExists) return res.status(400).json(DefaultErrors.NotExistsInDatase)
       const updatedClient = {
         ...verifyIfExists,
         // avatar: avatar.file.filename || verifyIfExists.avatar,
@@ -91,9 +85,7 @@ const ClientController = {
     try {
       const { id } = req.params
       const verifyIfExists = await Client.findByPk(id)
-      if (!verifyIfExists) {
-        return res.status(400).json(DefaultErrors.NotExistsInDatase)
-      }
+      if (!verifyIfExists) return res.status(400).json(DefaultErrors.NotExistsInDatase)
       await Client.destroy({ where: { id } })
       return res.status(204).json()
     } catch (err) {
