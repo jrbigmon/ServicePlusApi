4582210e2eb35f201f7c594eeb3050c90c22ffa2
jrbigmon <junioterriaga0@gmail.com>
2022-08-18 22:16:21 -0300

    Add middleware in router

--- src/router/AuthRouter.js
+++ src/router/AuthRouter.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const AuthController = require('../controller/AuthController')
+const verifyToken = require('../middleware/verifyToken')
 
-router.post('/login', AuthController.login)
+router.post('/login', verifyToken, AuthController.login)
 
 module.exports = router
