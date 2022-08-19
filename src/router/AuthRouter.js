c55a9d81d6c534c081915556e4d3a0112ef87f2b
jrbigmon <junioterriaga0@gmail.com>
2022-08-18 22:40:24 -0300

    Add query string in params uri

--- src/router/AuthRouter.js
+++ src/router/AuthRouter.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const AuthController = require('../controller/AuthController')
 const verifyToken = require('../middleware/verifyToken')
 
-router.post('/login', verifyToken, AuthController.login)
+router.post('/login?', verifyToken, AuthController.login)
 
 module.exports = router
