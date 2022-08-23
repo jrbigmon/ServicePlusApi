80c44520a21a4e1b8a125a2c12224bc9ba1a7dcc
jrbigmon <junioterriaga0@gmail.com>
2022-08-23 15:27:00 -0300

    Create admin's model

---
+++ src/model/Admin.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, DataTypes) => {
+    const Admin = sequelize.define('Admin', {
+      id: {
+        type: DataTypes.INTEGER(10),
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+  
+      email: {
+        type: DataTypes.STRING(200),
+        allowNull: false
+      },
+
+      password: {
+        type: DataTypes.STRING(500),
+        allowNull: false
+      },
+
+
+  
+    },
+    {
+      tableName: 'admin',
+      timestamps: false
+    })
+  
+    return Admin
+  }
+  
