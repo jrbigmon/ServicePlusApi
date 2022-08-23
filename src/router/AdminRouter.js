12a791b7ee9b8f5fe14b9c292e0e66bf4a6b14ac
jrbigmon <junioterriaga0@gmail.com>
2022-08-23 16:12:47 -0300

    Create Router admin

---
+++ src/router/AdminRouter.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router()
+const AdminController = require('../controller/AdminController')
+
+router.post('/admin', AdminController.createAdmin)
+
+module.exports = router
