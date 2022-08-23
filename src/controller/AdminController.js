41aa497dedcd6866be5e2702a11b0502da4776f2
jrbigmon <junioterriaga0@gmail.com>
2022-08-23 16:12:37 -0300

    BugFix registration admin in validate

--- src/controller/AdminController.js
+++ src/controller/AdminController.js
@@ -6,16 +6,19 @@ const AdminController = {
     createAdmin: async (req, res) => {
         try {
             const { email, password } = req.body
-            const newAdmin = { email, password }
+            const newAdmin = { 
+                email, 
+                password: !password.trim() ? "" : bcrypt.hashSync(password, 10) 
+            }
             for (let props in newAdmin){
                 const propertyWithoutSpace = newAdmin[props].trim()
                 if (!propertyWithoutSpace) return res.status(404).json(DefaultErrors.EmptyFields)
             }
             const verifyIfExists = await Admin.findOne({ where: { email }})
-            if(!verifyIfExists) return res.status(409).json(DefaultErrors.ExistsInDatase)
+            if(verifyIfExists) return res.status(409).json(DefaultErrors.ExistsInDatase)
             await Admin.create({
                 email: newAdmin.email,
-                password: bcrypt.hashSync(newAdmin.password.trim(), 10)
+                password: bcrypt.hashSync(newAdmin.password, 10)
             })
             return res.status(201).json({email, message: 'Success'})
         } catch (err) {
