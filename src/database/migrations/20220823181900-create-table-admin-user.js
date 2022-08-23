bb0c64c1de2ff04f02b749bc841b601f076986f0
jrbigmon <junioterriaga0@gmail.com>
2022-08-23 15:43:46 -0300

    Create migration to admin table

---
+++ src/database/migrations/20220823181900-create-table-admin-user.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('admin', { 
+      id: {
+        type: Sequelize.DataTypes.INTEGER(10),
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+
+      email: {
+        type: Sequelize.DataTypes.STRING(200),
+        allowNull: false
+      }, 
+      
+      password: {
+        type: Sequelize.DataTypes.STRING(500),
+        allowNull: false
+      } 
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('admin');
+  }
+};
