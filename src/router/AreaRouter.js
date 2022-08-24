61f4cf5ca5e104c30f46b3d577719d1237da483a
jrbigmon <junioterriaga0@gmail.com>
2022-08-23 21:10:50 -0300

    Added middeware to see available areas in the database, for testing

--- src/router/AreaRouter.js
+++ src/router/AreaRouter.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const AreaController = require('../controller/AreaController')
+const verifyToken = require('../middleware/verifyToken')
 
-router.get('/areas', AreaController.viewAreas)
+router.get('/areas', verifyToken, AreaController.viewAreas)
 router.post('/areas', AreaController.createArea)
 router.put('/areas/:id', AreaController.updateArea)
 router.delete('/areas/:id', AreaController.removeArea)
