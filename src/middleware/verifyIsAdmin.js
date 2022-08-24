717f5f8732a2a39276ffb2879dc3ac062eca24ff
jrbigmon <junioterriaga0@gmail.com>
2022-08-23 21:20:30 -0300

    Added response to client server

--- src/middleware/verifyIsAdmin.js
+++ src/middleware/verifyIsAdmin.js
@@ -6,8 +6,8 @@ const verifyIsAdmin = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1]
 
     const decode = jwt.verify(token, process.env.JWT_KEY)
-
-    if(decode.isAdmin !== 'enemySpots') return DefaultErrors.UserNotValidated
+    
+    if(decode.isAdmin !== 'enemySpots') return res.status(401).json(DefaultErrors.UserNotValidated)
     
     req.user = decode
 
