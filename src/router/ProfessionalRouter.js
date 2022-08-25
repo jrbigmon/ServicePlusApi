cf75247be0706929bae33153530fc4f484ab9b2e
jrbigmon <junioterriaga0@gmail.com>
2022-08-25 16:05:07 -0300

    Changed middleware usage mode

--- src/router/ProfessionalRouter.js
+++ src/router/ProfessionalRouter.js
@@ -3,10 +3,9 @@ const router = express.Router()
 const ProfessionalController = require('../controller/ProfessionalController')
 const verifyToken = require('../middleware/verifyToken')
 
-router.use(verifyToken)
-router.get('/professionals/:id', ProfessionalController.viewProfessional)
-router.post('/professionals', ProfessionalController.createProfessional)
-router.put('/professionals/:id', ProfessionalController.updateProfessional)
-router.delete('/professionals/:id', ProfessionalController.deleteProfessional)
+router.get('/professionals/:id', verifyToken, ProfessionalController.viewProfessional)
+router.post('/professionals', verifyToken, ProfessionalController.createProfessional)
+router.put('/professionals/:id', verifyToken, ProfessionalController.updateProfessional)
+router.delete('/professionals/:id', verifyToken, ProfessionalController.deleteProfessional)
 
 module.exports = router
