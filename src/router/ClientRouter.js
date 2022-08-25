2148856f446cf8f53c3862bc4812c40bbfc6882c
jrbigmon <junioterriaga0@gmail.com>
2022-08-25 15:48:42 -0300

    Change router position to post new client

--- src/router/ClientRouter.js
+++ src/router/ClientRouter.js
@@ -3,9 +3,10 @@ const router = express.Router()
 const ClientController = require('../controller/ClientController')
 const verifyToken = require('../middleware/verifyToken')
 
+router.post('/clients', ClientController.createClient)
+
 router.use(verifyToken)
 router.get('/clients/:id', ClientController.viewClient)
-router.post('/clients', ClientController.createClient)
 router.put('/clients/:id', ClientController.updateClient)
 router.delete('/clients/:id', ClientController.deleteClient)
 
