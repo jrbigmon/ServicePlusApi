9a844d659a04446d5074c87c1891316ec17cc956
jrbigmon <junioterriaga0@gmail.com>
2022-08-25 15:16:00 -0300

    Added verify token in service router

--- src/router/ServiceRouter.js
+++ src/router/ServiceRouter.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const ServiceController = require('../controller/ServiceController')
+const verifyToken = require('../middleware/verifyToken')
 
+router.use(verifyToken)
 router.get('/services/client/:id', ServiceController.viewAllServicesByClient)
 router.post('/services/client/:id', ServiceController.viewAllServicesByClient)
 
