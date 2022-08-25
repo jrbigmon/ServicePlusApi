f8e45941044e4b49c8a7e8cd2a1c86b32e477f82
jrbigmon <junioterriaga0@gmail.com>
2022-08-25 16:38:22 -0300

    Removed verifyToken to create new professional

--- src/router/ProfessionalRouter.js
+++ src/router/ProfessionalRouter.js
@@ -3,8 +3,8 @@ const router = express.Router()
 const ProfessionalController = require('../controller/ProfessionalController')
 const verifyToken = require('../middleware/verifyToken')
 
+router.post('/professionals', ProfessionalController.createProfessional)
 router.get('/professionals/:id', verifyToken, ProfessionalController.viewProfessional)
-router.post('/professionals', verifyToken, ProfessionalController.createProfessional)
 router.put('/professionals/:id', verifyToken, ProfessionalController.updateProfessional)
 router.delete('/professionals/:id', verifyToken, ProfessionalController.deleteProfessional)
 
