5d2c387162ddce34e01f5e6619ea1f7dce1ac6df
jrbigmon <junioterriaga0@gmail.com>
2022-08-25 15:26:59 -0300

    Change method of uselly verify is admin

--- src/router/AdminRouter.js
+++ src/router/AdminRouter.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const AdminController = require('../controller/AdminController')
 const verifyIsAdmin = require('../middleware/verifyIsAdmin')
 
-router.post('/admin', verifyIsAdmin, AdminController.createAdmin)
-router.delete('/admin/:id', verifyIsAdmin, AdminController.removeAdmin)
+router.use(verifyIsAdmin)
+router.post('/admin', AdminController.createAdmin)
+router.delete('/admin/:id', AdminController.removeAdmin)
 
 module.exports = router
