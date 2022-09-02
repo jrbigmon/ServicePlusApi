c47f3866779eb4b9b492fb6b3d4057c7f19a6dbb
jrbigmon <junioterriaga0@gmail.com>
2022-09-02 16:30:21 -0300

    Fixed middleware veirfyToken in routes

--- src/router/ServiceRouter.js
+++ src/router/ServiceRouter.js
@@ -7,10 +7,11 @@ router.get('/services/client/:id', verifyToken, ServiceController.viewAllService
 // router.post('/services/client/:id', verifyToken, ServiceController.viewAllServicesByClient)
 router.get('/services/professional/:id', verifyToken, ServiceController.viewAllServicesByProfessional)
 // router.post('/services/professional/:id', verifyToken, ServiceController.viewAllServicesByProfessional)
-router.post('/services/budget', verifyToken, ServiceController.requestBudget)
-router.put('/services/:id/budgeted', verifyToken, ServiceController.budgeted)
-router.put('/services/:id/accept', verifyToken, ServiceController.acceptBudget)
-router.put('/services/:id/finish', verifyToken, ServiceController.finishService)
-router.put('/services/:id/cancel', verifyToken, ServiceController.cancelService)
+router.use(verifyToken)
+router.post('/services/budget', ServiceController.requestBudget)
+router.put('/services/:id/budgeted', ServiceController.budgeted)
+router.put('/services/:id/accept', ServiceController.acceptBudget)
+router.put('/services/:id/finish', ServiceController.finishService)
+router.put('/services/:id/cancel', ServiceController.cancelService)
 
 module.exports = router
