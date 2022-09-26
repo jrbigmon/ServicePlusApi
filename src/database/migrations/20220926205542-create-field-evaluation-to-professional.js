e15884ceeb3b8ef52a6d01ee6b86f0e8415568b0
jrbigmon <juniorterriaga0@gmail.com>
2022-09-26 20:11:13 -0300

    Create migration to evaluation

---
+++ src/database/migrations/20220926205542-create-field-evaluation-to-professional.js
@@ -0,0 +1,58 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('evaluations_has_professionals', { 
+      id: {
+        type: Sequelize.DataTypes.INTEGER(10),
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+
+      professional_id: {
+        type: Sequelize.DataTypes.INTEGER(10),
+        references: {
+          model: { tableName: 'professionals' },
+          key: 'id'
+        },
+        allowNull: false
+      },
+
+      client_id: {
+        type: Sequelize.DataTypes.INTEGER(10),
+        references: {
+          model: { tableName: 'clients'},
+          key: 'id'
+        },
+        allowNull: false
+      },
+
+      assessment: {
+        type: Sequelize.DataTypes.INTEGER(10),
+        allowNull: true
+      },
+
+      created_at: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: true
+      },
+
+      updated_at: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: true
+      },
+
+      deleted_at: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: true
+      }
+
+    });
+    
+  },
+
+  async down (queryInterface, Sequelize) {  
+    await queryInterface.dropTable('evaluations_has_professionals');
+  }
+};
