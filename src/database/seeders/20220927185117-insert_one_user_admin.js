afe85d0f45e9b2acc72085144f32947a0da7761f
jrbigmon <juniorterriaga0@gmail.com>
2022-09-27 16:09:57 -0300

    added bcrypt to password admin

--- src/database/seeders/20220927185117-insert_one_user_admin.js
+++ src/database/seeders/20220927185117-insert_one_user_admin.js
@@ -1,11 +1,11 @@
 'use strict';
-
+const bcrypt = require('bcryptjs');
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('admin', [{
       id: 1,
       email: 'admin@example.com',
-      password: '1@3@4#2#4'
+      password: bcrypt.hashSync("1@3@4#2#4", 10)
     }], {});
   },
 
