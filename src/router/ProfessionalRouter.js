9cac50e419fd507c63619d9f48dc2cb9795dc33c
jrbigmon <juniorterriaga0@gmail.com>
2022-09-27 12:12:29 -0300

    Added new route to get all professionals with query string

--- src/router/ProfessionalRouter.js
+++ src/router/ProfessionalRouter.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const ProfessionalController = require('../controller/ProfessionalController')
 const verifyToken = require('../middleware/verifyToken')
 
+router.get('/professionals/?', ProfessionalController.getHigherRatingsProfessionals)
 router.post('/professionals', ProfessionalController.createProfessional)
 router.get('/professionals/:id', verifyToken, ProfessionalController.viewProfessional)
 router.put('/professionals/:id', verifyToken, ProfessionalController.updateProfessional)
