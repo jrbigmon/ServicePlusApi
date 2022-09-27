686cd7b55686d52e7a2d7237d6de0f09ee37691c
jrbigmon <juniorterriaga0@gmail.com>
2022-09-27 12:11:25 -0300

    Added new type error

--- src/Errors/DefaultErrors.js
+++ src/Errors/DefaultErrors.js
@@ -1,37 +1,44 @@
 module.exports = {
   ExistsInDatase: {
     error: true,
-    message: 'Exists in database already!'
+    message: 'Exists in database already!',
+    status: 409
   },
 
   NotExistsInDatase: {
     error: true,
-    message: 'Not Exists in database already!'
+    message: 'Not Exists in database already!',
+    status: 404
   },
 
   DatabaseOut: {
     error: true,
-    message: 'The system is out now!'
+    message: 'The system is out now!',
+    status: 500
   },
 
   BadRequestByUser: {
     error: true,
-    message: 'Bad request! Please try again.'
+    message: 'Bad request! Please try again.',
+    status: 400
   },
 
   EmptyFields: {
     error: true,
-    message: 'Has empty fields! Please try again.'
+    message: 'Has empty fields! Please try again.',
+    status: 400
   },
 
   UserNotValidated: {
     error: true,
-    message: 'User is not valid! Please try again.'
+    message: 'User is not valid! Please try again.',
+    status: 401
   },
 
   VerifyToken: {
     error: true,
-    message: 'Error in verify Token! Please try again.'
+    message: 'Error in verify Token! Please try again.',
+    status: 403
   }
 
 }
